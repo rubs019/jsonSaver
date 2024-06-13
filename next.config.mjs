530489710690293a9a6b15d0364490c71accb0a9
Ruben <ruben@empica.ch>
2024-06-13 09:52:17 +0200

    add basePath to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",
+    distDir: 'dist',
+    basePath: "/jsonSaver",
+};
 
 export default nextConfig;
